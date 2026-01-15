4d728d973839923f5d653ba5a7120b10a2edc1b5
Ruben <rubencasiraghi07@gmail.com>
2026-01-15 21:06:47 +0100

    New tournament changes

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
 from src.gamecontroller import GameController
 
 if __name__ == "__main__":
-    # Start game
     controller = GameController()
     if controller.ask_user():
         controller.play()
